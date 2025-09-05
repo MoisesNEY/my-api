a4bffa7c8c3a25c27fa3f291c8f4b9d40c08ef19
MoisesNEY <moisesney2@gmail.com>
2025-09-04 19:57:29 -0600

    Uso de lombok

--- src/main/java/my_api/models/User.java
+++ src/main/java/my_api/models/User.java
@@ -12,8 +12,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
 
@@ -32,90 +36,5 @@ public class User {
     @Column(name = "created_at")
     private Date createdAt;
 
-    // Constructor Vacio
-    public User() {
-    }
-
-    // Constructor para la clase User
-    public User(String username, String firstname, String lastname, String email, String password, List<String> roles) {
-        this.username = username;
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.email = email;
-        this.password = password;
-        this.roles = roles;
-        this.createdAt = new Date();
-    }
-
-    // Métodos get y set para ID
-    public Long getID() {
-        return id;
-    }
-
-    public void setID(Long id) {
-        this.id = id;
-    }
-
-    // Métodos get y set para username
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    // Métodos get y set para firstname
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    // Métodos get y set para lastname
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    // Métodos get y set para email
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    // Métodos get y set para password
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    // Métodos get y set para roles
-    public List<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }
-
-    // Métodos get y set para created_at
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
+    
 }
